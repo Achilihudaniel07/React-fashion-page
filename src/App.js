e84e8df8db32a134a1dd9c31437311f606c569ab
unknown <achilihudaniel53@gmail.com>
2022-08-10 21:04:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Body from './Body';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div>
       
       <Body/>
-           
+       <Footer/>    
     </div>
   );
 }
